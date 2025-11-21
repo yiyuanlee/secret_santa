916448580f80ee4229fc0f0c3c20eb5fae911a7f
Yiyuan Li <48795728+yiyuanlee@users.noreply.github.com>
2025-11-21 14:14:57 +0800

    Add main entry point for Secret Santa application

---
+++ mian.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SecretSantaApp from './SecretSantaApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SecretSantaApp />
+  </React.StrictMode>,
+)
